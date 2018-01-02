10d3d6dd596d70011c380e92ef7f6e9d413af4dd
malv8 <31816614+malv8@users.noreply.github.com>
2018-01-02 17:49:13 +0530

    Update Iris_data.R

--- Iris_data.R
+++ Iris_data.R
@@ -1,7 +1,6 @@
 #import built in dataset
 data("iris")
 attach(iris)
-
 #view summary of dataset
 summary(iris)
 sd(iris$Sepal.Length)
@@ -18,4 +17,14 @@ boxplot(iris)
 plot(ncurve,dnorm(ncurve,5.8 ,0.828),type = "l")
 plot(Species~Sepal.Length)
 plot(iris)
-t.test(Sepal.Length,Petal.Length,alternative = "greater")
+#import library
+library(dplyr)
+# filter() the data for species virginica
+virginica <- filter(iris, species == "virginica")
+head(virginica) # This dispalys the first six rows
+# select() the specified columns
+selected <- select(iris, sepal.length, sepal.width, petal.length)
+# select all columns from sepal.length to petal.length
+selected2 <- select(iris, sepal.length:petal.length)
+head(selected, 3)
+t.test(Sepal.Length,Petal.Length,alternative = "greater") #alternative hypothesis: true difference in means is greater than 0
